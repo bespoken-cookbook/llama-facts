1fdb8b64005961eb08e051e651d2eac29b1ee1e6
Juan Perata <juan@bespoken.tools>
2017-08-08 18:43:35 -0500

    Change from Pisco Facts to Llama Facts

--- languageStrings.js
+++ languageStrings.js
@@ -2,17 +2,17 @@ var languageStrings = {
     "en": {
         "translation": {
             "FACTS": [
-                "Pisco is a spirit made from the distillation of fermented grape juice.",
-                "In 2013 the EU moved to officially recognise pisco as a Peruvian spirit.",
-                "Most famous coctail made from Pisco is called Pisco Sour.",
-                "Pisco is the oldest distilled spirit in the Americas.",
-                "Pisco derives its name from the port in Peru where it originated, Puerto de Pisco.",
-                "Pisco Punch went on to gain worldwide fame after writers such as Mark Twain.",
-                "Hemingway was a huge fan of Pisco – and at one point, the Pisco Sour was his favourite drink."
+                "Llamas are large animals. They can be 6 feet tall and reach between 280 and 450 pounds of weight.",
+                "Llama's body is covered with wool which can be black, gray, white or brown, with variety of patterns.",
+                "Llama has excellent sense of sight, smell and hearing, which are used for detection of potential danger.",
+                "Llamas are very social animals. They live in groups composed of up to 20 animals.",
+                "Llamas are known as animals that spit when provoked or threatened. They can spit at humans also, but they usually spit at each other to solve misunderstandings in the group. ",
+                "Llamas are very intelligent, but stubborn animals. When the load on its back is too heavy, llama will refuse to carry it until some of the load is removed.",
+                "Lifespan of llamas is between 20 and 30 years."
             ],
-            "SKILL_NAME" : "Pisco Facts",
+            "SKILL_NAME" : "LLama Facts",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a pisco fact, or, you can say exit... What can I help you with?",
+            "HELP_MESSAGE" : "You can say tell me a llama fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
         }
