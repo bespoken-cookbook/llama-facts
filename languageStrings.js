9e256245b579e02f242fc2cec4c0eeea6d94375e
John Kelvie <john.kelvie@gmail.com>
2017-08-10 16:18:27 -0400

    Updated the fact text

--- languageStrings.js
+++ languageStrings.js
@@ -2,13 +2,13 @@ var languageStrings = {
     "en": {
         "translation": {
             "FACTS": [
-                "Llamas are large animals. They can be 6 feet tall and reach between 280 and 450 pounds of weight.",
-                "Llama's body is covered with wool which can be black, gray, white or brown, with variety of patterns.",
-                "Llama has excellent sense of sight, smell and hearing, which are used for detection of potential danger.",
+                "Llamas are large animals. They can be 6 feet tall and reach between 280 and 450 pounds.",
+                "A Llama's body is covered with wool which can be black, gray, white or brown, with variety of patterns.",
+                "Llamas have excellent sight, smell and hearing, which are used for detection of potential danger.",
                 "Llamas are very social animals. They live in groups composed of up to 20 animals.",
                 "Llamas are known as animals that spit when provoked or threatened. They can spit at humans also, but they usually spit at each other to solve misunderstandings in the group. ",
-                "Llamas are very intelligent, but stubborn animals. When the load on its back is too heavy, llama will refuse to carry it until some of the load is removed.",
-                "Lifespan of llamas is between 20 and 30 years."
+                "Llamas are very intelligent, but stubborn animals. When the load on its back is too heavy, a llama will refuse to carry it until some of the load is removed.",
+                "The lifespan of a llama is between 20 and 30 years."
             ],
             "SKILL_NAME" : "LLama Facts",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
